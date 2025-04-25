1ada58f7173e1e98e1de04471865cec90ac59499
whittenbrotherscom <marketing@whittenbrothers.com>
2025-04-25 11:38:38 -0400

    First Commit
    First Commit

---
+++ data.js
@@ -0,0 +1,818 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-01-whitten-mazda",
+      "name": "01-WHITTEN MAZDA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1376,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.7241348864117723
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8018490956300042,
+          "pitch": 0.0997348137970242,
+          "rotation": 0.7853981633974483,
+          "target": "2-03-front-view-parking"
+        },
+        {
+          "yaw": 0.3042108551362528,
+          "pitch": 0.13439409735232033,
+          "rotation": 0,
+          "target": "3-04-showroom-front-entrance"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.4490546320727127,
+          "pitch": 0.1327774335180969,
+          "title": "Whitten Mazda Virtual Tour Instructions - Click Here",
+          "text": "This tour can be viewed in complete 360 by holding the mouse or pointer and dragging the surroundings. To move on to the next area, click on the directional arrows. You can also move to different areas of the tour by clicking the title of the area from the list.<div><br></div><div>Company information can be found at: WWW.WHITTENMAZDA.COM</div>"
+        }
+      ]
+    },
+    {
+      "id": "1-02-front-view",
+      "name": "02-FRONT-VIEW",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1376,
+      "initialViewParameters": {
+        "yaw": 0.015864732901793133,
+        "pitch": -0.037064514194092624,
+        "fov": 1.8860134797570547
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.23744135908740205,
+          "pitch": 0.17410210849477004,
+          "rotation": 0,
+          "target": "3-04-showroom-front-entrance"
+        },
+        {
+          "yaw": 1.3847957063437768,
+          "pitch": 0.1800913036233709,
+          "rotation": 18.06415775814132,
+          "target": "12-13-service-drive"
+        },
+        {
+          "yaw": -1.4723044866992208,
+          "pitch": 0.17856771746343902,
+          "rotation": 11.780972450961727,
+          "target": "0-01-whitten-mazda"
+        },
+        {
+          "yaw": -3.0164382375908083,
+          "pitch": 0.28089593927990464,
+          "rotation": 6.283185307179586,
+          "target": "2-03-front-view-parking"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-03-front-view-parking",
+      "name": "03-FRONT-VIEW-PARKING",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1376,
+      "initialViewParameters": {
+        "yaw": 0.01589876127726697,
+        "pitch": 0.10473712889906928,
+        "fov": 1.7241348864117723
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.010901327297629493,
+          "pitch": 0.20840016323940702,
+          "rotation": 0,
+          "target": "1-02-front-view"
+        },
+        {
+          "yaw": -1.9783572930095161,
+          "pitch": 0.18887380080549576,
+          "rotation": 6.283185307179586,
+          "target": "0-01-whitten-mazda"
+        },
+        {
+          "yaw": 0.7486889653959157,
+          "pitch": 0.15936635334546523,
+          "rotation": 0.7853981633974483,
+          "target": "12-13-service-drive"
+        },
+        {
+          "yaw": -0.7872082231212616,
+          "pitch": 0.1447854211237427,
+          "rotation": 4.71238898038469,
+          "target": "0-01-whitten-mazda"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-04-showroom-front-entrance",
+      "name": "04-SHOWROOM-FRONT-ENTRANCE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1376,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7668972010423563,
+          "pitch": 0.308517997745561,
+          "rotation": 5.497787143782138,
+          "target": "7-08-lounge"
+        },
+        {
+          "yaw": 0.7171819405381576,
+          "pitch": 0.327859945915856,
+          "rotation": 0.7853981633974483,
+          "target": "5-06-showroom-corner"
+        },
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "4-05-showroom-center"
+        },
+        {
+          "yaw": -3.028260691585567,
+          "pitch": 0.3180719305408246,
+          "rotation": 0,
+          "target": "1-02-front-view"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-05-showroom-center",
+      "name": "05-SHOWROOM-CENTER",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1376,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5681929155936256,
+          "pitch": 0.27660877188924005,
+          "rotation": 0,
+          "target": "7-08-lounge"
+        },
+        {
+          "yaw": -0.7831625958694488,
+          "pitch": 0.2404381041808712,
+          "rotation": 11.780972450961727,
+          "target": "8-09-hallway-front"
+        },
+        {
+          "yaw": 0.4648104146903016,
+          "pitch": 2.5934939529292933e-8,
+          "rotation": 0,
+          "target": "6-07-showroom-rear"
+        },
+        {
+          "yaw": 1.7472511487756295,
+          "pitch": 0.2970175344635724,
+          "rotation": 0,
+          "target": "5-06-showroom-corner"
+        },
+        {
+          "yaw": -3.109920724313074,
+          "pitch": 0.25126188791773707,
+          "rotation": 0,
+          "target": "3-04-showroom-front-entrance"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-06-showroom-corner",
+      "name": "06-SHOWROOM-CORNER",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1376,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8007849158896665,
+          "pitch": 0.10584671853203531,
+          "rotation": 0.7853981633974483,
+          "target": "3-04-showroom-front-entrance"
+        },
+        {
+          "yaw": 1.8829927650134248,
+          "pitch": 0.21555079139527322,
+          "rotation": 0,
+          "target": "4-05-showroom-center"
+        },
+        {
+          "yaw": 2.885584651335072,
+          "pitch": -0.026311019172595707,
+          "rotation": 0,
+          "target": "6-07-showroom-rear"
+        },
+        {
+          "yaw": -2.6732669462674856,
+          "pitch": 0.027738194953389694,
+          "rotation": 0,
+          "target": "15-16-service-hall"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-07-showroom-rear",
+      "name": "07-SHOWROOM-REAR",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1376,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5987355173463316,
+          "pitch": 0.377275852824436,
+          "rotation": 0,
+          "target": "15-16-service-hall"
+        },
+        {
+          "yaw": -2.851879815044974,
+          "pitch": 0.1084332425143586,
+          "rotation": 0.7853981633974483,
+          "target": "5-06-showroom-corner"
+        },
+        {
+          "yaw": 3.132056650637111,
+          "pitch": -0.027343246401667187,
+          "rotation": 0,
+          "target": "4-05-showroom-center"
+        },
+        {
+          "yaw": -1.5622724653929687,
+          "pitch": 0.23947737450563267,
+          "rotation": 0,
+          "target": "10-11-restrooms"
+        },
+        {
+          "yaw": -0.052277933062249815,
+          "pitch": 0.24738832955991086,
+          "rotation": 0,
+          "target": "11-12-parts-counter"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-08-lounge",
+      "name": "08-LOUNGE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1376,
+      "initialViewParameters": {
+        "yaw": -0.03613347273008216,
+        "pitch": 0.0789558119732181,
+        "fov": 1.7241348864117723
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.622363446121943,
+          "pitch": 0.22455027099981706,
+          "rotation": 0,
+          "target": "8-09-hallway-front"
+        },
+        {
+          "yaw": -3.1065888963467216,
+          "pitch": 0.17542248735909105,
+          "rotation": 0,
+          "target": "4-05-showroom-center"
+        },
+        {
+          "yaw": -2.2839413447121295,
+          "pitch": 0.19506732882638822,
+          "rotation": 0,
+          "target": "3-04-showroom-front-entrance"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-09-hallway-front",
+      "name": "09-HALLWAY-FRONT",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1376,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.013080633138695674,
+          "pitch": 0.3081487706656354,
+          "rotation": 0,
+          "target": "9-10-hallway-rear"
+        },
+        {
+          "yaw": 3.125014968576421,
+          "pitch": 0.18922260372566413,
+          "rotation": 0,
+          "target": "7-08-lounge"
+        },
+        {
+          "yaw": 2.7113581325802016,
+          "pitch": 0.36894015701194505,
+          "rotation": 5.497787143782138,
+          "target": "4-05-showroom-center"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-10-hallway-rear",
+      "name": "10-HALLWAY-REAR",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1376,
+      "initialViewParameters": {
+        "yaw": 2.2229413745324074,
+        "pitch": 0.2755393136227937,
+        "fov": 1.7241348864117723
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4982586544113108,
+          "pitch": 0.4640868684744568,
+          "rotation": 6.283185307179586,
+          "target": "10-11-restrooms"
+        },
+        {
+          "yaw": 3.117748402529415,
+          "pitch": 0.26877025694656353,
+          "rotation": 0,
+          "target": "8-09-hallway-front"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-11-restrooms",
+      "name": "11-RESTROOMS",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1376,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.010900748310200825,
+          "pitch": 0.2456490443234447,
+          "rotation": 0,
+          "target": "6-07-showroom-rear"
+        },
+        {
+          "yaw": -3.131805676963836,
+          "pitch": 0.4838575189433918,
+          "rotation": 0,
+          "target": "9-10-hallway-rear"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-12-parts-counter",
+      "name": "12-PARTS-COUNTER",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1376,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1279782499008437,
+          "pitch": 0.20815005545094323,
+          "rotation": 0,
+          "target": "6-07-showroom-rear"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-13-service-drive",
+      "name": "13-SERVICE-DRIVE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1376,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.041400812790989505,
+          "pitch": 0.18098058570523534,
+          "rotation": 0,
+          "target": "13-14-service-lane"
+        },
+        {
+          "yaw": -2.854980471423488,
+          "pitch": 0.1537431042486137,
+          "rotation": 0.7853981633974483,
+          "target": "1-02-front-view"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-14-service-lane",
+      "name": "14-SERVICE-LANE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1376,
+      "initialViewParameters": {
+        "yaw": -0.8686486964787221,
+        "pitch": -0.016113410153380414,
+        "fov": 1.7241348864117723
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5865018702583562,
+          "pitch": 0.2608177135206091,
+          "rotation": 0,
+          "target": "14-15-service-desk"
+        },
+        {
+          "yaw": 3.1243230150106562,
+          "pitch": 0.28721366340711896,
+          "rotation": 0,
+          "target": "12-13-service-drive"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-15-service-desk",
+      "name": "15-SERVICE-DESK",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1376,
+      "initialViewParameters": {
+        "yaw": -0.8672033455217409,
+        "pitch": 0.0032226820306746617,
+        "fov": 1.7241348864117723
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.083137973085737,
+          "pitch": 0.404839215680747,
+          "rotation": 0,
+          "target": "15-16-service-hall"
+        },
+        {
+          "yaw": 1.5312286818568275,
+          "pitch": 0.25923511635260255,
+          "rotation": 0,
+          "target": "13-14-service-lane"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "15-16-service-hall",
+      "name": "16-SERVICE-HALL",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1376,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.32000635715773385,
+          "pitch": 0.3728504748172021,
+          "rotation": 0,
+          "target": "14-15-service-desk"
+        },
+        {
+          "yaw": -1.504136661431442,
+          "pitch": 0.4304534594184499,
+          "rotation": 0,
+          "target": "6-07-showroom-rear"
+        },
+        {
+          "yaw": 2.9789632810128346,
+          "pitch": 0.06713408529033238,
+          "rotation": 0,
+          "target": "5-06-showroom-corner"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "WHITTEN MAZDA VIRTUAL TOUR",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
